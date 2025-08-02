8cacf218714c3b53780b6b3420b8fc8924f54fb0
mbalcer <mateusz75319@gmail.com>
2025-08-02 21:16:43 +0200

    Initial commit

---
+++ src/main/java/pl/mbalcer/luxmedreservation/LuxmedReservationApplication.java
@@ -0,0 +1,13 @@
+package pl.mbalcer.luxmedreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxmedReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxmedReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mbalcer/luxmedreservation/LuxmedReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mbalcer.luxmedreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxmedReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

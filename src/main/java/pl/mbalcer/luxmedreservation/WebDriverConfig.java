918643e67975603e6ab6f41e65a97af56075d760
mbalcer <mateusz75319@gmail.com>
2025-08-02 21:17:14 +0200

    Create webdriver config

---
+++ src/main/java/pl/mbalcer/luxmedreservation/WebDriverConfig.java
@@ -0,0 +1,30 @@
+package pl.mbalcer.luxmedreservation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class WebDriverConfig {
+
+    @Bean
+    public WebDriver webDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        return driver;
+    }
+}

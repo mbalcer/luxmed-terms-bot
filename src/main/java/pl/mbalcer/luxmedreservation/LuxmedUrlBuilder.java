81f484316bb60f8efe841804f118d61c62091e57
mbalcer <mateusz75319@gmail.com>
2025-08-10 21:41:51 +0200

    Create url builder to request check terms

---
+++ src/main/java/pl/mbalcer/luxmedreservation/LuxmedUrlBuilder.java
@@ -0,0 +1,66 @@
+package pl.mbalcer.luxmedreservation;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuxmedUrlBuilder {
+    private static final String BASE_URL = "https://portalpacjenta.luxmed.pl/PatientPortal/NewPortal/terms/index";
+
+    private Integer searchPlaceId;
+    private Integer serviceVariantId;
+    private LocalDate searchDateFrom;
+    private LocalDate searchDateTo;
+    private Boolean delocalized;
+    private List<Integer> doctorsIds = new ArrayList<>();
+
+    public LuxmedUrlBuilder searchPlaceId(int id) {
+        this.searchPlaceId = id;
+        return this;
+    }
+
+    public LuxmedUrlBuilder serviceVariantId(int id) {
+        this.serviceVariantId = id;
+        return this;
+    }
+
+    public LuxmedUrlBuilder searchDateFrom(LocalDate date) {
+        this.searchDateFrom = date;
+        return this;
+    }
+
+    public LuxmedUrlBuilder searchDateTo(LocalDate date) {
+        this.searchDateTo = date;
+        return this;
+    }
+
+    public LuxmedUrlBuilder delocalized(boolean value) {
+        this.delocalized = value;
+        return this;
+    }
+
+    public LuxmedUrlBuilder addDoctorId(int id) {
+        this.doctorsIds.add(id);
+        return this;
+    }
+
+    public String build() {
+        if (searchPlaceId == null || serviceVariantId == null || searchDateFrom == null || searchDateTo == null || delocalized == null) {
+            throw new IllegalStateException("Brak wymaganych parametrów");
+        }
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(BASE_URL)
+                .queryParam("searchPlace.id", searchPlaceId)
+                .queryParam("serviceVariantId", serviceVariantId)
+                .queryParam("searchDateFrom", searchDateFrom)
+                .queryParam("searchDateTo", searchDateTo)
+                .queryParam("delocalized", delocalized);
+
+        doctorsIds.forEach(id -> builder.queryParam("doctorsIds", id));
+
+        return builder.build().toUriString();
+    }
+}
+
--- src/main/java/pl/mbalcer/luxmedreservation/TermsService.java
+++ src/main/java/pl/mbalcer/luxmedreservation/TermsService.java
@@ -11,6 +11,7 @@ import pl.mbalcer.luxmedreservation.model.TermsInfoForDay;
 import pl.mbalcer.luxmedreservation.notification.MessageBuilder;
 import pl.mbalcer.luxmedreservation.notification.TelegramNotifier;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -37,16 +38,21 @@ public class TermsService {
             cookieHeader.append(cookie.getName()).append("=").append(cookie.getValue()).append("; ");
         }
 
-// Creating RestClient with default headers
         RestClient restClient = RestClient.builder()
                 .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0")
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
-// URL with parameters for checking appointments
-        String url = "https://portalpacjenta.luxmed.pl/PatientPortal/NewPortal/terms/index?searchPlace.id=10&searchPlace.name=Bydgoszcz&searchPlace.type=0&serviceVariantId=4461&languageId=10&searchDateFrom=2025-08-12&searchDateTo=2025-08-28&doctorsIds=15665&doctorsIds=37970&nextSearch=false&delocalized=false";
+        String url = new LuxmedUrlBuilder()
+                .searchPlaceId(10)
+                .serviceVariantId(4461)
+                .searchDateFrom(LocalDate.now().plusDays(1))
+                .searchDateTo(LocalDate.now().plusDays(15))
+                .addDoctorId(15665)
+                .addDoctorId(37970)
+                .delocalized(false)
+                .build();
 
-// Making the request with RestClient
         MedicalAppointmentResponse response = restClient.get()
                 .uri(url)
                 .header("Cookie", cookieHeader.toString())

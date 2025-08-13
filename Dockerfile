cf2f5f0cc95d8c53ab5c13fd79795c548db000e6
mbalcer <mateusz75319@gmail.com>
2025-08-13 22:16:43 +0200

    Replace the web driver configuration that uses the Bonigarcia library on a remote Chrome running in Docker

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
 
-FROM selenium/standalone-chrome:latest
+FROM eclipse-temurin:21-jdk
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 EXPOSE 8080
-CMD ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/pl/mbalcer/luxmedreservation/WebDriverConfig.java
+++ src/main/java/pl/mbalcer/luxmedreservation/WebDriverConfig.java
@@ -1,22 +1,24 @@
 package pl.mbalcer.luxmedreservation;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import java.time.Duration;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class WebDriverConfig {
 
     @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public WebDriver webDriver() {
+    public WebDriver webDriver(@Value("${SELENIUM_REMOTE_URL}") String seleniumUrl) throws URISyntaxException, MalformedURLException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
@@ -24,10 +26,6 @@ public class WebDriverConfig {
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-allow-origins=*");
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-
-        return driver;
+        return new RemoteWebDriver(new URI(seleniumUrl).toURL(), options);
     }
 }
